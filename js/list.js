cdb92ddfa545fb8b7ba09358d1acc08599d089fd
huyduc98 <d>
2024-12-02 21:17:30 +0700

    no message

--- js/list.js
+++ js/list.js
@@ -11,3 +11,20 @@ fetch("https://learn-be-37y5.onrender.com/auth/getForm", requestOptions)
   .then((result) => console.log(result))
   .catch((error) => console.error(error))
 }
+
+const images = document.querySelectorAll('.gallery-item');
+
+// Thêm sự kiện click cho từng ảnh để nhấp vào
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        // Hiển thị overlay
+        document.getElementById('overlay').style.display = 'flex';
+        // Đặt nguồn ảnh vào img zoomed
+        document.getElementById('zoomedImage').src = image.src;
+    });
+});
+
+// Hàm để đóng overlay khi nhấp vào
+function closeZoom() {
+    document.getElementById('overlay').style.display = 'none';
+}
